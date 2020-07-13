3ad1a2f5f7da920199d74a2c7fc19016c22a4ff8
Guillaume Lempereur <guigui2993@users.noreply.github.com>
2020-07-13 15:53:57 +0200

    Update updateStats.php

--- updateStats.php
+++ updateStats.php
@@ -14,7 +14,7 @@ try
 	if(isset($_POST["AnswerStats"])){
 	    $answerStats = json_decode($_POST["AnswerStats"],true);
 	    // insert update
-		$req = $bdd->prepare('INSERT INTO Answer(USER, WORD, LANG, DATE, TRUE_POS, TRUE_NEG, FALSE_POS, FALSE_NEG, SCORE_1, SCORE) VALUES(:userID, :wordID, :Lang, NOW(), :TP, :TN, :FP, :FN, :score, :score *0.5) ON DUPLICATE KEY UPDATE TRUE_POS = TRUE_POS + :TP, TRUE_NEG = TRUE_NEG + :TN, FALSE_POS = FALSE_POS + :FP, FALSE_NEG = FALSE_NEG + :FN, SCORE = :score*0.5+SCORE_1*0.3+SCORE_2*0.2, SCORE_3 = SCORE_2, SCORE_2 = SCORE_1, SCORE_1 = :score, DATE = NOW()');
+		$req = $bdd->prepare('INSERT INTO Answer(USER, WORD, LANG, DATE, TRUE_POS, TRUE_NEG, FALSE_POS, FALSE_NEG, SCORE_1, SCORE) VALUES(:userID, :wordID, :Lang, NOW(), :TP, :TN, :FP, :FN, :score, :score *0.5) ON DUPLICATE KEY UPDATE TRUE_POS = TRUE_POS + :TP, TRUE_NEG = TRUE_NEG + :TN, FALSE_POS = FALSE_POS + :FP, FALSE_NEG = FALSE_NEG + :FN, SCORE = :score*0.25+SCORE_1*0.15+SCORE_2*0.1+ SCORE_ORTHOGR_1*0.25+SCORE_ORTHOGR_2*0.15+SCORE_ORTHOGR_3*0.1, SCORE_3 = SCORE_2, SCORE_2 = SCORE_1, SCORE_1 = :score, DATE = NOW()');
 
 		foreach($answerStats as $wordID => $stat){
 			$score = $stat[4];
@@ -36,6 +36,23 @@ try
 		    $req->execute($answer);
 		}
 	}
+	
+	if(isset($_POST["answerTypeStats"])){
+	    $answerTypeStats = json_decode($_POST["answerTypeStats"],true);
+	    // insert update
+		// new insertion score = 0.5*0 (MCQ) + 0.5*(Type score *0.5)
+		$req = $bdd->prepare('INSERT INTO Answer(USER, WORD, LANG, DATE, SCORE_ORTHOGR_1, SCORE) VALUES(:userID, :wordID, :Lang, NOW(), :score, :score *0.25) ON DUPLICATE KEY UPDATE SCORE = SCORE_1*0.25+SCORE_2*0.15+SCORE_3*0.1+:score*0.25+SCORE_ORTHOGR_1*0.15+SCORE_ORTHOGR_2*0.1, SCORE_ORTHOGR_3 = SCORE_ORTHOGR_2, SCORE_ORTHOGR_2 = SCORE_ORTHOGR_1, SCORE_ORTHOGR_1 = :score, DATE = NOW()');
+
+		foreach($answerTypeStats as $wordID => $score){
+			if($score == -1){ // no score skip
+				continue;
+			}
+
+			$answer = ["userID" => $userID, "Lang" => $L_Lang, "wordID" => $wordID, "score" => $score];
+		    $req->execute($answer);
+		}
+	}
+	
 	
 	echo "Stats updated";
 }
