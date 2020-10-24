f12ad5bf82676bbab5dffd8d042b749d09d7e30d
Guillaume Lempereur <guigui2993@users.noreply.github.com>
2020-10-24 22:55:32 +0200

    update vocLearn.js

--- vocLearn/vocLearn.js
+++ vocLearn/vocLearn.js
@@ -37,9 +37,26 @@ function generateMCQ_template(words){
 	);
 }
 
-var progress = 0; 
+var progress = -1; // -1 not in a game otherwise 0 to 100
+var level = 0;
 var difficultyLabels = ["Easy", "Intermediate", "Hard", "Extreme"];
 $("#difficulty").change(fonction(){
-  
-  $("#difficultyLabel").html(difficultyLabels[$(this).value]);
+  if(progress != -1){
+    msg("modification not yet supported during a game");
+    $(this).value = level;
+  }else{
+    level =$(this).value;
+    $("#difficultyLabel").html(difficultyLabels[level]);
+  }
 });
+
+function msg(text){
+  $("#msg").html(txt).show();
+  // timeout
+  
+  $("#msg").hide();
+}
+
+function generateMCQ(){
+  
+}
